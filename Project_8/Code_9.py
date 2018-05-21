12835449105158a8c7e73349b9895c21c36890f5
ksarpang <kishan.sarpangala@hotmail.com>
2018-05-21 13:21:20 -0400

    Deep Learning Project 8
    Deep learning Project 8

---
+++ Project_8/Code_9.py
@@ -0,0 +1,99 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+
+tf.reset_default_graph()
+#reset_graph()
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("/tmp/data/")
+
+restore_saver = tf.train.import_meta_graph("./my_best_mnist_model_0_to_4.meta")
+
+X = tf.get_default_graph().get_tensor_by_name("X:0")
+y = tf.get_default_graph().get_tensor_by_name("y:0")
+loss = tf.get_default_graph().get_tensor_by_name("loss:0")
+Y_proba = tf.get_default_graph().get_tensor_by_name("Y_proba:0")
+logits = Y_proba.op.inputs[0]
+accuracy = tf.get_default_graph().get_tensor_by_name("accuracy:0")
+
+learning_rate = 0.01
+
+output_layer_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="logits")
+optimizer = tf.train.AdamOptimizer(learning_rate, name="Adam2")
+training_op = optimizer.minimize(loss, var_list=output_layer_vars)
+
+correct = tf.nn.in_top_k(logits, y, 1)
+accuracy = tf.reduce_mean(tf.cast(correct, tf.float32), name="accuracy")
+
+init = tf.global_variables_initializer()
+five_frozen_saver = tf.train.Saver()
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("/tmp/data/")
+
+X_train2_full = mnist.train.images[mnist.train.labels >= 5]
+y_train2_full = mnist.train.labels[mnist.train.labels >= 5] - 5
+X_valid2_full = mnist.validation.images[mnist.validation.labels >= 5]
+y_valid2_full = mnist.validation.labels[mnist.validation.labels >= 5] - 5
+X_test2 = mnist.test.images[mnist.test.labels >= 5]
+y_test2 = mnist.test.labels[mnist.test.labels >= 5] - 5
+
+
+def sample_n_instances_per_class(X, y, n=100):
+    Xs, ys = [], []
+    for label in np.unique(y):
+        idx = (y == label)
+        Xc = X[idx][:n]
+        yc = y[idx][:n]
+        Xs.append(Xc)
+        ys.append(yc)
+    return np.concatenate(Xs), np.concatenate(ys)
+
+
+X_train2, y_train2 = sample_n_instances_per_class(X_train2_full, y_train2_full, n=100)
+X_valid2, y_valid2 = sample_n_instances_per_class(X_valid2_full, y_valid2_full, n=30)
+
+import time
+
+n_epochs = 1000
+batch_size = 20
+
+max_checks_without_progress = 20
+checks_without_progress = 0
+best_loss = np.infty
+
+with tf.Session() as sess:
+    init.run()
+    restore_saver.restore(sess, "./my_best_mnist_model_0_to_4")
+    for var in output_layer_vars:
+        var.initializer.run()
+
+    t0 = time.time()
+
+    for epoch in range(n_epochs):
+        rnd_idx = np.random.permutation(len(X_train2))
+        for rnd_indices in np.array_split(rnd_idx, len(X_train2) // batch_size):
+            X_batch, y_batch = X_train2[rnd_indices], y_train2[rnd_indices]
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+        loss_val, acc_val = sess.run([loss, accuracy], feed_dict={X: X_valid2, y: y_valid2})
+        if loss_val < best_loss:
+            save_path = five_frozen_saver.save(sess, "./my_mnist_model_5_to_9_five_frozen")
+            best_loss = loss_val
+            checks_without_progress = 0
+        else:
+            checks_without_progress += 1
+            if checks_without_progress > max_checks_without_progress:
+                print("Early stopping!")
+                break
+        print("{}\tValidation loss: {:.6f}\tBest loss: {:.6f}\tAccuracy: {:.2f}%".format(
+            epoch, loss_val, best_loss, acc_val * 100))
+
+    t1 = time.time()
+    print("Total training time: {:.1f}s".format(t1 - t0))
+
+with tf.Session() as sess:
+    five_frozen_saver.restore(sess, "./my_mnist_model_5_to_9_five_frozen")
+    acc_test = accuracy.eval(feed_dict={X: X_test2, y: y_test2})
+    print("Final test accuracy: {:.2f}%".format(acc_test * 100))
